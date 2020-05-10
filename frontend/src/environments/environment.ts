25c96a8bc408f284bb9fc5864a1f0b0b2a23ce0e
trendysloth <ww296@cornell.edu>
2020-05-10 14:10:47 -0700

    https to http

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -9,6 +9,6 @@ export const environment = {
     url: 'fsndkally', // the auth0 domain prefix
     audience: 'coffee', // the audience set for the auth0 app
     clientId: '1VBgvWf2ycvA8dfBxpWwZDHuVJ7GihCY', // the client id generated for the auth0 app
-    callbackURL: 'https://127.0.0.1:8100', // the base url of the running ionic application. 
+    callbackURL: 'http://127.0.0.1:8100', // the base url of the running ionic application. 
   }
 };
