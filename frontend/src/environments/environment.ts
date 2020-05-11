3a84819a499503f7194637566730d8c86c8ced73
trendysloth <ww296@cornell.edu>
2020-05-10 19:15:09 -0700

    changed to localhost

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,11 +4,11 @@
 
 export const environment = {
   production: false,
-  apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
+  apiServerUrl: 'http://localhost:5000', // the running FLASK api server url
   auth0: {
     url: 'fsndkally', // the auth0 domain prefix
     audience: 'coffee', // the audience set for the auth0 app
     clientId: '1VBgvWf2ycvA8dfBxpWwZDHuVJ7GihCY', // the client id generated for the auth0 app
-    callbackURL: 'http://127.0.0.1:8100', // the base url of the running ionic application. 
+    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
